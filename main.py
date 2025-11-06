0f25cdb5d611adfec0efff0454fc6d4ad0158bb0
bumen <113213270+b316da@users.noreply.github.com>
2025-11-06 03:46:55 +0300

    Introduce real-time notifications

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T00:46:25.303Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2025-11-06T00:46:54.281Z
+Commit: Introduce real-time notifications
