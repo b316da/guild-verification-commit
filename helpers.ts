2846fb2d9e7106a1e6e35ef206e38b13bc794efc
bumen <113213270+b316da@users.noreply.github.com>
2025-11-06 03:48:57 +0300

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T00:47:09.912Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2025-11-06T00:48:55.614Z
+Commit: Resolve null pointer exception in user service
