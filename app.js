dff86a451277338f56b0d6137b57f831d476cdbf
bumen <113213270+b316da@users.noreply.github.com>
2025-11-06 03:31:25 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T00:31:21.759Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2025-11-06T00:31:24.560Z
+Commit: Fix broken links in documentation
