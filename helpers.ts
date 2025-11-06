ee9342c65d3e94edc01e3602416e412a8c72a7d7
bumen <113213270+b316da@users.noreply.github.com>
2025-11-06 03:45:29 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T00:45:26.867Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2025-11-06T00:45:28.336Z
+Commit: Patch API endpoint error handling
