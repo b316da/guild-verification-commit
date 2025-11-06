b5b1039c5590f3fbdd99a78b338790b9c709d03e
bumen <113213270+b316da@users.noreply.github.com>
2025-11-06 03:32:19 +0300

    Add multi-language support

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T00:32:06.434Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2025-11-06T00:32:18.561Z
+Commit: Add multi-language support
