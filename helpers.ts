2471d68fd49efc4d3ff5d747ff74ff9662072a86
bumen <113213270+b316da@users.noreply.github.com>
2025-11-06 03:34:33 +0300

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T00:34:15.636Z
-Commit: Patch API endpoint error handling
+Timestamp: 2025-11-06T00:34:32.673Z
+Commit: Fix broken links in documentation
