8366ededb5338cd107597a4b997cc8005420fa97
bumen <113213270+b316da@users.noreply.github.com>
2025-11-06 03:45:28 +0300

    Resolve timezone issues in date handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T00:45:18.412Z
-Commit: Implement user profile customization
+Timestamp: 2025-11-06T00:45:26.867Z
+Commit: Resolve timezone issues in date handling
