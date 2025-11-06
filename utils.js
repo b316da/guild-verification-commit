d57f4ecf4fa5a4091f78ae0364c414ac3e171602
bumen <113213270+b316da@users.noreply.github.com>
2025-11-06 03:47:27 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T00:46:11.975Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2025-11-06T00:47:26.608Z
+Commit: Fix memory leak in event handlers
