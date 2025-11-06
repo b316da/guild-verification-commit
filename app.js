48f7fe2cac5c5c66e02dad83b9a97289f171bbd4
bumen <113213270+b316da@users.noreply.github.com>
2025-11-06 03:31:35 +0300

    Resolve null pointer exception in user service

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T00:31:31.761Z
-Commit: Patch API endpoint error handling
+Timestamp: 2025-11-06T00:31:34.500Z
+Commit: Resolve null pointer exception in user service
