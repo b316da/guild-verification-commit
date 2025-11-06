ab014245d66ae493235e1bb2d47eef3850fa2857
bumen <113213270+b316da@users.noreply.github.com>
2025-11-06 03:49:44 +0300

    Add multi-language support

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T00:49:34.248Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2025-11-06T00:49:43.608Z
+Commit: Add multi-language support
