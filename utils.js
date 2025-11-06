3517a395a67fac98828342c2e20f2a1667f9356b
bumen <113213270+b316da@users.noreply.github.com>
2025-11-06 03:49:32 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T00:49:30.026Z
-Commit: Add multi-language support
+Timestamp: 2025-11-06T00:49:31.583Z
+Commit: Resolve null pointer exception in user service
