3720f53e17c90f863cc09986c325eb35769c73fb
bumen <113213270+b316da@users.noreply.github.com>
2025-11-06 03:32:37 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T00:32:30.800Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2025-11-06T00:32:36.173Z
+Commit: Fix memory leak in event handlers
