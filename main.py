89c94d122fdc7dc39c34c506f6148453930ac075
bumen <113213270+b316da@users.noreply.github.com>
2025-11-06 03:30:50 +0300

    Introduce real-time notifications

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T00:30:47.553Z
-Commit: Add export functionality for reports
+Timestamp: 2025-11-06T00:30:48.972Z
+Commit: Introduce real-time notifications
