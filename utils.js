b7a3f6e70f456491e1a86ccb4693c241d8bbbd28
bumen <113213270+b316da@users.noreply.github.com>
2025-11-06 03:32:32 +0300

    Resolve timezone issues in date handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T00:32:25.513Z
-Commit: Introduce real-time notifications
+Timestamp: 2025-11-06T00:32:30.800Z
+Commit: Resolve timezone issues in date handling
