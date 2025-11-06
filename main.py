0284b26598080c714dd62bc2603c9d0562078a17
bumen <113213270+b316da@users.noreply.github.com>
2025-11-06 03:30:58 +0300

    Fix memory leak in event handlers

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T00:30:48.972Z
-Commit: Introduce real-time notifications
+Timestamp: 2025-11-06T00:30:57.728Z
+Commit: Fix memory leak in event handlers
